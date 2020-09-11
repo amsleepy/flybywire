a91c5b3cc667385e8c0ac675ff159206ad433b14
amsleepy <sa154124@gmail.com>
2020-09-10 21:09:24 -0700

    added pa1

--- programmingAssign1/Benchmarking.java
+++ programmingAssign1/Benchmarking.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Benchmarking {
 
@@ -13,37 +15,65 @@ public class Benchmarking {
         LinkedList<Integer> num = new LinkedList<Integer>();
         BufferedReader read = null;
 
+        Scanner keys = new Scanner(System.in);
+        System.out.println("Enter name of file to be sorted.");
+        String filename = keys.nextLine();
+        System.out.println();
+
+        long start = System.nanoTime();
+
         try { 
 
-            read = new BufferedReader(new FileReader("input1.txt"));
-
-            String head = read.readLine();
-            String next;
-            int numberA = Integer.parseInt(head);
-            int numberB;
-            
-            num.addFirst(numberA);
+            read = new BufferedReader(new FileReader(filename));
 
             while (read.readLine() != null) {
 
-                next = read.readLine();
-                numberA = Integer.parseInt(head);
-                numberB = Integer.parseInt(next);
-
-                if (numberB <= numberA) {
-                    num.addFirst(numberB);
-                } else {
-                    num.add(numberB);
-                }
+                String insert = read.readLine();
+                num.add(Integer.parseInt(insert));
+                Collections.sort(num, (Integer one, Integer two) -> one.compareTo(two));
 
             }
+
+            long end = System.nanoTime();
+
+            System.out.println("Values inserted in " + (end - start)/1000000 + " milliseconds.");
+            System.out.println("----------------------------------------------");
+            num.forEach(Integer -> System.out.println(Integer));
+
+            System.out.println();
+
+            start = System.nanoTime();
+            int min = num.get(0);
+            end = System.nanoTime();
+            System.out.println("Minimum found in " + (end - start)/1000 + " microseconds.");
+            System.out.println("List minimum = " + min + "\n");
+
+            start = System.nanoTime();
+            int max = num.getLast();
+            end = System.nanoTime();
+            System.out.println("Maximum found in " + (end - start)/1000 + " microseconds.");
+            System.out.println("List maximum = " + max + "\n");
+
+            start = System.nanoTime();
+            int med = num.get(num.size()/2);
+            end = System.nanoTime();
+            System.out.println("Median found in " + (end - start)/1000 + " microseconds.");
+            System.out.println("List median = " + med + "\n");
+
+
 
         } catch (FileNotFoundException f) {
             f.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } finally {
 
-    }
-    
+            try {
+                if (read != null) {
+                    read.close();
+                } 
+            } catch (IOException e) {
+            } 
+        }
+    }   
 }
